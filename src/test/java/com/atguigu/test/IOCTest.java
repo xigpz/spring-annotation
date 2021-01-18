f9aeb8e3c21ac49247e2296f041d62777dfbe282
xigpz <1023348836@qq.com>
2021-01-18 14:56:32 +0800

    学习

--- src/test/java/com/atguigu/test/IOCTest.java
+++ src/test/java/com/atguigu/test/IOCTest.java
@@ -1,11 +1,19 @@
 package com.atguigu.test;
 
+import java.util.Map;
+
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+import com.atguigu.bean.Blue;
+import com.atguigu.bean.Person;
 import com.atguigu.config.MainConfig;
+import com.atguigu.config.MainConfig2;
 
 public class IOCTest {
+	AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig2.class);
+	
 	@SuppressWarnings("resource")
 	@Test
 	public void test01() {
@@ -15,6 +23,49 @@ public class IOCTest {
 			System.out.println(name);
 		}
 		
+	}
+	
+	@Test
+	public void test02() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig2.class);
+//		String[] definitionNames = applicationContext.getBeanDefinitionNames();
+//		for (String name : definitionNames) {
+//			System.out.println(name);
+//		}
+		System.out.println("ioc容器创建完成...");
+		Object bean = applicationContext.getBean("person");
+		Object bean2 = applicationContext.getBean("person");
+		System.out.println(bean == bean2);
 		
+		
+	} 
+	
+	@Test
+	public void test03() {
+		 String[] nameForTypes = applicationContext.getBeanNamesForType(Person.class);
+		 ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		 //获取环境变量的值
+		 String property = environment.getProperty("os.name");
+		 //System.out.println(property);
+		 for (String name : nameForTypes) {
+			System.out.println(name);
+		}
+		 Map<String, Person> persons = applicationContext.getBeansOfType(Person.class);
+		 System.out.println(persons);
+		 
+	}
+	
+	@Test
+	public void testImport() {
+		printBeans(applicationContext);
+		Blue bean = applicationContext.getBean(Blue.class);
+		System.out.println(bean);
+	}
+	
+	private void printBeans(AnnotationConfigApplicationContext applicationContext) {
+		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+		for (String name : beanDefinitionNames) {
+			System.out.println(name);
+		}
 	}
 }
